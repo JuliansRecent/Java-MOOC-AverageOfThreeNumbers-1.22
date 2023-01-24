8d2b55cf972d2f300dd5612e5e747b4ea579adc4
JuliansRecent <juliansrecent@gmail.com>
2023-01-23 18:43:27 -0800

    Module complete. Prompt the user for three numbers. Calculate the average of the three numbers and print the result to the console.

--- src/main/java/AverageOfThreeNumbers.java
+++ src/main/java/AverageOfThreeNumbers.java
@@ -1,4 +1,6 @@
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class AverageOfThreeNumbers {
@@ -7,6 +9,16 @@ public class AverageOfThreeNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        // Prompt the user for three numbers
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int thirdNumber = Integer.valueOf(scanner.nextLine());
+        // Calculate the average and turn it into a double type
+        double average = (1.0 * (firstNumber + secondNumber + thirdNumber)) / 3;
+        // Print the average to the console
+        System.out.println("The average is " + average);
     }
 }
